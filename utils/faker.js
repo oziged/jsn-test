c2f5230f5600b0e9c9e0cb9caf6d3da1e0d85a33
oziged <oziged@gmail.com>
2020-04-24 12:30:19 +0300

    images links fix

--- utils/faker.js
+++ utils/faker.js
@@ -8,6 +8,6 @@ export function generateHeroes() {
     origin_description: faker.lorem.sentence(5),
     superpowers: faker.lorem.words(3),
     catch_phrase: faker.company.catchPhrase(),
-    images: [...Array(5)].map(img => `https://i.picsum.photos/id/${Math.round(Math.random() * 1000)}/500/500.jpg`)
+    images: [...Array(5)].map(img => `https://i.picsum.photos/id/${Math.round(Math.random() * 10) * 100}/500/500.jpg`)
   }))
 }
