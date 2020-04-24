39545f66f0f641a9a196b3a72e8b890d18609bd5
oziged <oziged@gmail.com>
2020-04-24 10:54:45 +0300

    minor fixes

---
+++ utils/faker.js
@@ -0,0 +1,13 @@
+import faker from 'faker'
+
+export function generateHeroes() {
+  return [...Array(50)].map((item, index) => ({
+    id: index,
+    nickname: faker.internet.userName().slice(0,5),
+    real_name: faker.name.firstName(),
+    origin_description: faker.lorem.sentence(5),
+    superpowers: faker.lorem.words(3),
+    catch_phrase: faker.company.catchPhrase(),
+    images: [...Array(5)].map(img => `https://i.picsum.photos/id/${Math.round(Math.random() * 1000)}/500/500.jpg`)
+  }))
+}
